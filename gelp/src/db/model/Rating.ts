3fdd2c6d3738b079e921e3c61281c8333b4c79de
Manu chowdary Kalluri <manuchowdarykalluri@Manus-MacBook-Air-2.local>
2026-02-12 10:32:20 -0500

    feat(db): add Game ref to rating schema

--- gelp/src/db/model/Rating.ts
+++ gelp/src/db/model/Rating.ts
@@ -17,10 +17,12 @@ const ratingSchema = new mongoose.Schema<IRating>({
     index: true,
   },
   game: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    index: true, // intentionally no ref
-  },
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "Game",
+  required: true,
+  index: true,
+},
+
   score: {
     type: mongoose.Schema.Types.Number,
     required: true,
