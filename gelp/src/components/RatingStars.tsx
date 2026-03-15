72b908457f70e2332ddf40950af695923f3c6101
Manu chowdary Kalluri <manuchowdarykalluri@Manus-Air-2.lan>
2026-03-15 16:58:33 -0400

    add rating stars and rating submission

---
+++ gelp/src/components/RatingStars.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+type Props = {
+  onChange?: (rating: number) => void;
+};
+
+export default function RatingStars({ onChange }: Props) {
+  const [rating, setRating] = useState(0);
+
+  const handleClick = (value: number) => {
+    setRating(value);
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
+  return (
+    <div style={{ fontSize: "30px" }}>
+      {[1, 2, 3, 4, 5].map((star) => (
+        <span
+          key={star}
+          onClick={() => handleClick(star)}
+          style={{
+            cursor: "pointer",
+            color: star <= rating ? "gold" : "gray",
+          }}
+        >
+          ★
+        </span>
+      ))}
+    </div>
+  );
+}
