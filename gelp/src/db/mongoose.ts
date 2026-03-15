2d026307a414905bd4b3b84d45a92d249880f77f
Manu chowdary Kalluri <manuchowdarykalluri@Manus-Air-2.lan>
2026-03-15 12:22:20 -0400

    Fixed mongoose runtime issue and verified local setup

--- gelp/src/db/mongoose.ts
+++ gelp/src/db/mongoose.ts
@@ -1,10 +1,11 @@
-import assert from "node:assert";
 import * as mongoose from "mongoose";
 
-assert(process.env.MONGO_URI, 'env:MONGO_URI is not set.');
+if (!process.env.MONGO_URI) {
+  throw new Error("env:MONGO_URI is not set.");
+}
 
 const connection = mongoose.connect(process.env.MONGO_URI);
 
 export const ensureMongoose = () => {
   return connection;
-}
+};
