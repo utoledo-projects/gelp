10dd3bcac18c28621255f88556c2f7514d6704e7
Zach Baker <zach@zachbaker.dev>
2026-04-07 10:15:59 -0400

    feat: use user hook and user context (#50)

---
+++ gelp/src/hooks/useUser.ts
@@ -0,0 +1,10 @@
+"use client";
+
+import {useContext} from "react";
+import UserContext from "@/context/UserContext";
+
+const useUser = () => {
+  return useContext(UserContext);
+}
+
+export default useUser;
