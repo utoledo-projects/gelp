54eed619118640e79fe582814f3dd3cbcc842781
Zach Baker <zach@zachbaker.dev>
2026-04-14 21:54:46 -0400

    feat: profile page and avatar logic (#87)

--- gelp/src/app/(auth_optional)/game/[gameID]/GameRatings.tsx
+++ gelp/src/app/(auth_optional)/game/[gameID]/GameRatings.tsx
@@ -19,13 +19,13 @@ const GameRatings: FC<GameRatingsProps> = ({ratings}) => {
     <h1 className='text-3xl'>Ratings</h1>
     {ratings.count === 0 && <div className='flex flex-col gap-4'>
       <p>No ratings yet.</p>
-      {user === null && <p><Link href='/auth/login'>Sign in</Link> to rate this game.</p>}
+      {user === null && <p><Link className='underline' href='/auth/login'>Sign in</Link> to rate this game.</p>}
       {user !== null && <button className='bg-blue-600 rounded-lg py-1 px-2'>Be the first to rate this game!</button>}
     </div>}
     {ratings.count > 0 && <div>
       <NewRatingsStarsDisplay sum={ratings.sum} count={ratings.count}/>
     </div>}
-    {user === null && <p><Link href='/auth/login'>Sign in</Link> to rate this game.</p>}
+    {user === null && <p><Link className='underline' href='/auth/login'>Sign in</Link> to rate this game.</p>}
     {user !== null && <button className='bg-blue-600 rounded-lg py-1 px-2'>Rate this game</button>}
   </div>;
 }
